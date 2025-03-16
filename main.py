e643866e751748eb06c95d71f7b4b0113e55f1d1
Taniastep <55vivax@mail.ru>
2025-03-16 21:33:36 +0300

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import pandas as pd
-import json
-
 def clo():
-    new_dict = dict()
-with open('/content/mature.fa','r') as f:
-  for line in f:
-    m = line.strip()
-    if len(m) != 0 and m[0] == '>':
-      m = m.split()
-      key = m[0][1:]
-      new_dict[key] = ""
-    else:
-        new_dict[key] += m
-for key, value in new_dict.items():
-#  print(key, '*', value)):
+    ict = dict()
+    with open('C:/_project/refseq_transcript.fasta','r') as f:
+      for line in f:
+         m = line.strip()
+         if len(m) != 0 and m[0] == '>':
+           m = m.split()
+           key = m[0][1:]
+           ict[key] = ""
+         else:
+           ict[key] += m
+
+      for key, value in ict.items():
+         print(key, '*', value)
+
+clo()
